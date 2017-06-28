129723bd63f80bdb3900dccfd8cb9842dee4f426
Sergey Mironov <grrwlf@gmail.com>
2017-06-28 19:05:24 +0300

    Version 1.8.1

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@ let
       }:
       mkDerivation {
         pname = "VKHS";
-        version = "1.8";
+        version = "1.8.1";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
